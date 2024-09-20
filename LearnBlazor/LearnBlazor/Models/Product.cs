813b56562df3f9eac37bd0644981cf426cd3bda9
Vivek <vivek01110110@gmail.com>
2024-09-20 21:30:02 +0530

    feat: added basic blazor project

---
+++ LearnBlazor/LearnBlazor/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace LearnBlazor.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsActive {  get; set; }
+
+        public double Price { get; set; }
+        public IEnumerable<Product_Prop> ProductProperties { get; set; }
+    }
+}
